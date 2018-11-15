11901b45b7cc0435cb4a0d39411d6510c820e0eb
John McFarlane <github@john.mcfarlane.name>
2018-11-15 21:58:00 +0000

    Add _impl::duplex_integer numeric type
    - pairs of integers acting as a single, wider integer

---
+++ include/cnl/_impl/duplex_integer/set_digits.h
@@ -0,0 +1,21 @@
+
+//          Copyright John McFarlane 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(CNL_IMPL_DUPLEX_INTEGER_SET_DIGITS_H)
+#define CNL_IMPL_DUPLEX_INTEGER_SET_DIGITS_H
+
+#include "forward_declaration.h"
+#include "../num_traits/set_digits.h"
+
+/// compositional numeric library
+namespace cnl {
+    template<typename Upper, typename Lower, int Digits>
+    struct set_digits<_impl::duplex_integer<Upper, Lower>, Digits> {
+        using word = _impl::to_rep_t<Upper>;
+    };
+}
+
+#endif  // CNL_IMPL_DUPLEX_INTEGER_SET_DIGITS_H
