006fc5b38131c8c5c38ca1a1b0dd057daa7a7bd0
John McFarlane <github@john.mcfarlane.name>
2018-11-22 20:06:27 +0000

    Integrate multiword_integer into wide_integer
    - many changes to tests in fixed_point_common.h
    - tests of integration between fixed_point and wide_integer backed by
      int8, int32 and int64

--- include/cnl/_impl/wide_integer/type.h
+++ include/cnl/_impl/wide_integer/type.h
@@ -8,6 +8,9 @@
 #define CNL_IMPL_WIDE_INTEGER_TYPE_H
 
 #include "../integer/type.h"
+#include "../multiword_integer/numeric_limits.h"
+#include "../multiword_integer/optimal_multiword_integer.h"
+#include "../multiword_integer/type.h"
 #include "../num_traits/max_digits.h"
 #include "../num_traits/set_digits.h"
 #include "../number_base.h"
@@ -18,16 +21,21 @@
 namespace cnl {
     namespace _impl {
         // wide_integer_rep
-        template<int Digits, typename Narrowest, typename Enable = void>
+        template<int Digits, typename Narrowest, bool SupportMultiply, typename Enable = void>
         struct wide_integer_rep;
 
-        template<int Digits, typename Narrowest>
-        struct wide_integer_rep<Digits, Narrowest, enable_if_t<(max_digits<Narrowest>::value>=Digits)>>
+        template<int Digits, typename Narrowest, bool SupportMultiply>
+        struct wide_integer_rep<Digits, Narrowest, SupportMultiply, enable_if_t<(max_digits<Narrowest>::value>=Digits)>>
                 : set_digits<Narrowest, max(Digits, digits<Narrowest>::value)> {
         };
 
-        template<int Digits, typename Narrowest>
-        using wide_integer_rep_t = typename wide_integer_rep<Digits, Narrowest>::type;
+        template<int Digits, typename Narrowest, bool SupportMultiply>
+        struct wide_integer_rep<Digits, Narrowest, SupportMultiply, enable_if_t<(max_digits<Narrowest>::value<Digits)>>
+                : optimal_multiword_integer<Digits, Narrowest, SupportMultiply> {
+        };
+
+        template<int Digits, typename Narrowest, bool SupportMultiply = true>
+        using wide_integer_rep_t = typename wide_integer_rep<Digits, Narrowest, SupportMultiply>::type;
 
         // wide_integer
         template<int Digits, typename Narrowest>
